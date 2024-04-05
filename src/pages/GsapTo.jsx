8dbbd477ace8ede2345efd4b8325361a255f2a4a
habib33-3 <habib.swi16@gmail.com>
2024-04-06 01:57:35 +0600

    gsap.to

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation:360,
+      duration:2,
+      ease: "expo"
+    });
+  }, []);
 
   return (
     <main>
@@ -30,7 +43,10 @@ const GsapTo = () => {
       </p>
 
       <div className="mt-20">
-        <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg" />
+        <div
+          id="blue-box"
+          className="w-20 h-20 bg-blue-500 rounded-lg"
+        />
       </div>
     </main>
   );
